6a6cd26aba64b6f3309456749b95d408ddaf4da1
PolAmador99 <64004266+PolAmador99@users.noreply.github.com>
2020-04-21 10:21:38 +0200

    0.1.0
    -Movimiento basico del personaje

---
+++ Mecanica_Personaje/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Collider
+    private IsGrounded feed = null;
+    //Rigidbody
+    private Rigidbody rb;
+
+    //Variables
+    public int MoveSpeed;
+    public int RotateSpeed;
+    public int JumpForce;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        feed = GameObject.Find("Feed").GetComponent<IsGrounded>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+        Jump();
+
+        
+
+        
+
+    }
+
+
+
+    private void Move()
+    {
+        // Movimiento 
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.Translate((Vector3.forward * MoveSpeed) * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.Translate((Vector3.forward * -MoveSpeed) * Time.deltaTime);
+
+        }
+    }
+
+    private void Rotate()
+    {
+
+        // Rotacion
+
+       if (Input.GetKey(KeyCode.A))
+       {
+
+          transform.Rotate((Vector3.up * RotateSpeed) * Time.deltaTime);
+
+       }
+
+       if (Input.GetKey(KeyCode.D))
+       {
+
+          transform.Rotate((Vector3.up * -RotateSpeed) * Time.deltaTime);
+
+       }
+
+    }
+
+    private void Jump()
+    {
+        // Salto
+
+        if (Input.GetKey(KeyCode.Space) && feed.is_grouned) rb.AddForce((Vector3.up * JumpForce), ForceMode.Impulse);
+        
+    }
+}
---
+++ Mecanica_Personaje/Assets/Scripts/IsGrounded.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsGrounded : MonoBehaviour
+{
+
+    //Variables
+    [HideInInspector] public bool is_grouned;
+
+    
+    void Awake()
+    {
+
+        is_grouned = false;
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag != "Player") is_grouned = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != "Player") is_grouned = false;
+    }
+}
